e8b9120b8f1f4b30d8b671a6db84f1d21f163d13
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-03-20 09:53:01 +0300

    Add toastify to show notification alerts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Register from './components/auth/Register';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
   render() {
-    return <Register />;
+    return (
+      <Fragment>
+        <ToastContainer />
+        <Register />;
+      </Fragment>
+    );
   }
 }
 
