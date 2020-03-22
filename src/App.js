31d1b71f1f7b228559bdb98eebbcf9216a26ec3c
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-03-22 20:17:17 +0300

    Add register route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <ToastContainer />
-        <Register />;
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Register} />
+        </Switch>
       </Fragment>
     );
   }
