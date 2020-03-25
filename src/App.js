870466c90bdca7a7cc344944c8ec9e31d5c64b97
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-03-25 17:07:55 +0300

    modify files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+// import Login from './components/auth/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
         <ToastContainer />
-        <Switch>
-          <Route path='/login' component={Login} />
-          <Route path='/' component={Register} />
-        </Switch>
+        <Router>
+          <Switch>
+            {/* <Route path='/login' component={Login} /> */}
+            <Route path='/' component={Register} />
+          </Switch>
+        </Router>
       </Fragment>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    {' '}
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
