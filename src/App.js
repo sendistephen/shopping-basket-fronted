41fe919578881528e856e3a4d0d79e42b683d7d6
Ssendikadiwa Stephen <ssendikadiwastephen@Ssendikadiwas-MacBook-Pro.local>
2020-04-03 20:13:39 +0300

    Modify login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import Register from './components/auth/Register';
-// import Login from './components/auth/Login';
+import Login from './components/auth/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Baskets from './components/baskets/baskets';
 
 class App extends Component {
   render() {
@@ -13,8 +19,10 @@ class App extends Component {
         <ToastContainer />
         <Router>
           <Switch>
-            {/* <Route path='/login' component={Login} /> */}
-            <Route path='/' component={Register} />
+            <Route exact path='/baskets' component={Baskets} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/' component={Register} />
+            <Redirect exact from='/login' to='/baskets' />
           </Switch>
         </Router>
       </Fragment>
